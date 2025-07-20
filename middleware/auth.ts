b8cb94226adb93dfa12478e4fbeff0e96c32d292
Christian Kyle <111234084+riskyle@users.noreply.github.com>
2025-07-20 19:17:47 +0800

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,13 +4,13 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const publicPages = ['/login', '/register', '/confirm-email'];
     const isPublicPage = publicPages.includes(to.path);
-    const isAuthenticated = !!user;
+    const isAuthenticated = !!user.value;
 
     if (!isPublicPage && !isAuthenticated) {
         return navigateTo('/login');
     }
 
-    if (isPublicPage && user) {
+    if (isPublicPage && user.value) {
         return navigateTo('/');
     }
 })
